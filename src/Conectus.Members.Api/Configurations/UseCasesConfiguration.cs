71698dc93b35747e4b1f54c0db440b0c404dfcd2
Helberth Versiani <helberthlucas@gmail.com>
2025-11-13 19:55:05 -0300

    Add EndToEndTests project and UseCases configuration
    Added a new `Conectus.Members.EndToEndTests` project to the solution, targeting .NET 8.0, with xUnit and code coverage support. Updated the solution file to include build configurations and nested the project under the appropriate parent.
    
    Introduced a `UseCasesConfiguration` class to streamline service and repository registration, including MediatR, `IMemberRepository`, and `IUnitOfWork`.
    
    Improved solution structure for better modularity, testing, and dependency injection.

---
+++ src/Conectus.Members.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Conectus.Members.Application.Interfaces;
+using Conectus.Members.Application.UseCases.Member.CreateMember;
+using Conectus.Members.Domain.Repository;
+using Conectus.Members.Infra.Data.EF;
+using Conectus.Members.Infra.Data.EF.Repositories;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Conectus.Members.Api.Configurations
+{
+    public static class UseCasesConfiguration
+    {
+        public static IServiceCollection AddUseCases(
+    this IServiceCollection services
+)
+        {
+            services.AddMediatR(cfg =>
+               cfg.RegisterServicesFromAssembly(typeof(Program).Assembly)
+               );
+            services.AddRepositories();
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(
+                this IServiceCollection services
+            )
+        {
+            services.AddTransient<IMemberRepository, MemberRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
